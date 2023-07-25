4fa1fc9888b4fc2f8d348155cc1c12229d82c531
OpeDada <opedada@gmail.com>
2023-07-25 23:54:46 +0900

    basic nav info

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Nav />
-      <Main />
-      <Footer />
+      {/* <Main /> */}
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,16 @@
 
 const Nav = () => {
   return (
-    <>
-    </>
-  )
+    <nav className="py-4 px-8 text-green font-bold">
+      <div className="container mx-auto">
+        <ul className="flex space-x-4">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav
