0143ad4a963c5230b7e7a9dfdab249b9d65e51d5
OpeDada <opedada@gmail.com>
2023-07-25 23:44:32 +0900

    install and configure taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
