9866a27b734c3ed0f399d0fa3efe75906b8d29cb
OpeDada <opedada@gmail.com>
2023-08-09 23:23:53 +0900

    design header section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <main className="py-8">
-      <header className="bg-hero bg-cover h-64"></header>
-    </main>
+    <>
+      <main className="">
+        <header className="bg-hero bg-cover h-60 px-32 py-4">
+          <h1 className="text-yellow text-3xl font-serif font-bold">
+            Little Lemon
+          </h1>
+          <h4 className="text-white text-xl">Chicago</h4>
+          <p className="text-white w-80 py-4">
+            We are a family owned Mediterranean restaurant, located on Maldove
+            Street in Chicago, Illionis. We focus on traditional recipes served
+            with a modern twist.
+          </p>
+        </header>
+        {/* <Link className="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4" to="/">
+          Reserve a Table
+        </Link> */}
+        <h3 className="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4">
+          Reserve a Table
+        </h3>
+      </main>
+    </>
   );
 };
 
