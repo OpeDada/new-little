c9f94f5b0c8e09a781f0b4021d389804a76d209c
OpeDada <opedada@gmail.com>
2023-08-13 23:11:44 +0900

    create specials section

--- src/components/pages/Homepage/Specials.js
+++ src/components/pages/Homepage/Specials.js
@@ -2,15 +2,26 @@ const Specials = () => {
   return (
     <div className="px-20 py-40">
       <div className="flex justify-between">
-        <h1>This weeks specials!</h1>
+        <h1 className="text-3xl font-bold">This weeks specials!</h1>
         <button className="bg-yellow hover:bg-green hover:text-white hover:border border-gray font-bold py-2 px-4 rounded">
           Online Menu
         </button>
       </div>
       <section className="cards">
-        <div className="card">
-          <img></img>
-        </div>
+        <article class="card">
+          <img src="/hair_lady.jpg" alt="" />
+          <div class="card-info">
+            <h2>Greek salad</h2>
+            <p>$12.99</p>
+            <p>
+              Refreshing salad, made with tomato, lettuce, feta cheese, and
+              olives. Dressed with salt, hot pepper, and olive oil.
+            </p>
+            <button className="bg-yellow hover:bg-green hover:text-white hover:border border-gray font-bold py-2 px-4 rounded">
+              Order for Delivery
+            </button>
+          </div>
+        </article>
       </section>
     </div>
   );
