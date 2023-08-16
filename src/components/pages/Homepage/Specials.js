1e1761f57e0785c8caad354155192906f19545ef
OpeDada <opedada@gmail.com>
2023-08-16 15:06:45 +0900

    completed specials section

--- src/components/pages/Homepage/Specials.js
+++ src/components/pages/Homepage/Specials.js
@@ -1,4 +1,6 @@
 import greek from "../../../assets/greek salad.jpg";
+import bruchetta from "../../../assets/bruchetta.svg";
+import lemonDessert from "../../../assets/lemon_d.jpg";
 
 const Specials = () => {
   return (
@@ -11,13 +13,51 @@ const Specials = () => {
       </div>
       <section className="cards py-8 grid grid-cols-3 gap-6">
         <article class="card max-w-sm bg-gray-light rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <img src={greek} alt="greek salad" />
-          <div class="card-info">
-            <h2>Greek salad</h2>
-            <p>$12.99</p>
-            <p>
+          <img src={greek} alt="greek salad" className="rounded-t-lg" />
+          <div class="card-info px-6 py-4 ">
+            <div className="flex justify-between py-4 font-bold">
+              <h2>Greek salad</h2>
+              <p className="text-orange ">$12.99</p>
+            </div>
+            <p className="pb-8">
               Refreshing salad, made with tomato, lettuce, feta cheese, and
               olives. Dressed with salt, hot pepper, and olive oil.
+            </p>
+            <button className="bg-yellow hover:bg-green hover:text-white hover:border border-gray font-bold py-2 px-4 rounded">
+              Order for Delivery
+            </button>
+          </div>
+        </article>
+        <article class="card max-w-sm bg-gray-light rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img src={bruchetta} alt="bruchetta" className="rounded-t-lg" />
+          <div class="card-info px-6 py-4 ">
+            <div className="flex justify-between py-4 font-bold">
+              <h2>Bruchetta</h2>
+              <p className="text-orange ">$16.99</p>
+            </div>
+            <p className="pb-8">
+              Toasted bread, topped with tomato, prosciutto, and cheese.
+              Seasoned with salt and olive oil.
+            </p>
+            <button className="bg-yellow hover:bg-green hover:text-white hover:border border-gray font-bold py-2 px-4 rounded">
+              Order for Delivery
+            </button>
+          </div>
+        </article>
+        <article class="card max-w-sm bg-gray-light rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img
+            src={lemonDessert}
+            alt="lemon dessert"
+            className="rounded-t-lg"
+          />
+          <div class="card-info px-6 py-4">
+            <div className="flex justify-between py-4 font-bold">
+              <h2>Lemon Dessert</h2>
+              <p className="text-orange ">$8.50</p>
+            </div>
+            <p className="pb-8">
+              Fresh baked lemon bread coated in salt and sugar. Powdered in
+              citrus and lemon zest.
             </p>
             <button className="bg-yellow hover:bg-green hover:text-white hover:border border-gray font-bold py-2 px-4 rounded">
               Order for Delivery
