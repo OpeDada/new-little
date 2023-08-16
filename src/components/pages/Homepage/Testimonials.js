232ad239cd6c3fde5f07cb3b5d1eb6b2559e9434
OpeDada <opedada@gmail.com>
2023-08-16 16:15:11 +0900

    added content for testimonials section

--- src/components/pages/Homepage/Testimonials.js
+++ src/components/pages/Homepage/Testimonials.js
@@ -1,5 +1,244 @@
 const Testimonials = () => {
-  return <></>;
+  return (
+    <>
+      <div classNameName="container my-24 mx-auto md:px-6">
+        <section className="mb-32 text-center py-16">
+          <h2 className="mb-16 text-3xl font-bold">Testimonials</h2>
+
+          <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12">
+            <div className="mb-12 md:mb-0">
+              <div className="mb-6 flex justify-center">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+                  className="w-32 rounded-full shadow-lg dark:shadow-black/20"
+                  alt=""
+                />
+              </div>
+              <h5 className="mb-2 text-lg font-bold">Maria Smantha</h5>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                eos id officiis hic tenetur quae quaerat ad velit ab hic.
+              </p>
+              <ul className="mb-0 flex justify-center text-yellow">
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m480 757 157 95-42-178 138-120-182-16-71-168v387ZM233 976l65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-12 md:mb-0">
+              <div className="mb-6 flex justify-center">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).jpg"
+                  className="w-32 rounded-full shadow-lg dark:shadow-black/20"
+                  alt=""
+                />
+              </div>
+              <h5 className="mb-2 text-lg font-bold">Lisa Cudrow</h5>
+              <p className="mb-4">
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                corporis suscipit laboriosam, nisi ut aliquid commodi.
+              </p>
+              <ul className="mb-0 flex justify-center text-yellow">
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-0">
+              <div className="mb-6 flex justify-center">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).jpg"
+                  className="w-32 rounded-full shadow-lg dark:shadow-black/20"
+                  alt=""
+                />
+              </div>
+              <h5 className="mb-2 text-lg font-bold">John Smith</h5>
+              <p className="mb-4">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque.
+              </p>
+              <ul className="mb-0 flex justify-center text-yellow">
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 96 960 960"
+                    className="w-5 text-warning"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m323 851 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178Zm-90 125 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-355Z"
+                    />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Testimonials;
