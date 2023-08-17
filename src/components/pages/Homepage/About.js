1f1213ada043d9d71e2a923c4e11276e5cbf216a
OpeDada <opedada@gmail.com>
2023-08-17 23:30:32 +0900

    added content for about me section

--- src/components/pages/Homepage/About.js
+++ src/components/pages/Homepage/About.js
@@ -1,7 +1,16 @@
 const About = () => {
   return (
     <>
-<h1>Little Lemon Chicago</h1>
+      <h1>Little Lemon Chicago</h1>
+      <h2>Chicago</h2>
+      <p className="">
+        Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario.
+        Despite the city's diversity, the two brothers recognized the lack of
+        Mediterranean cuisine in Chicago, and were inspired to bring the flavors
+        of their hometown in Italy to the people of Chicago. The two brothers
+        continue to oversee the Little Lemon restaurant, nearly thirty years
+        later.
+      </p>
     </>
   );
 };
