c4e2e5ca9aa4300fc83fcdf3bf7964f96e21f096
OpeDada <opedada@gmail.com>
2023-08-30 11:30:16 +0900

    added routes components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Homepage from "./components/pages/Homepage/Homepage";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="mx-auto">
-      <Nav />
-      <Homepage />
-      <Footer />
-    </div>
+    <Router>
+      <div className="mx-auto">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
