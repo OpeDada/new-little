7f9cbc4099d3b111a2c92b9ec218d499e9cbbb9c
OpeDada <opedada@gmail.com>
2023-11-23 14:40:28 +0900

    created hamburger nav for smaller screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,94 @@
+import React, { useState } from "react";
 import logo from "../assets/Logo.svg";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="px-20 xl:px-60 py-4 text-green font-bold flex items-center">
       <img src={logo} alt="Nav logo"></img>
       <div className="container mx-auto">
-        <ul className="flex justify-end md:space-x-4 lg:space-x-8">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/reservation">Reservations</Link>
-          </li>
-          <li>
-            <Link to="/order">Order</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <div className="justify-end hidden md:flex md:items-center">
+          <ul className="flex justify-end md:space-x-4 lg:space-x-8">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/reservation">Reservations</Link>
+            </li>
+            <li>
+              <Link to="/order">Order</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="md:hidden flex justify-end items-center">
+          <button
+            onClick={toggleMenu}
+            className="outline-none focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 text-gray-700"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {showMenu ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        {showMenu && (
+          <ul className="md:hidden flex flex-col items-center mt-4">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/reservation">Reservations</Link>
+            </li>
+            <li>
+              <Link to="/order">Order</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
-}
+};
 
-export default Nav
-
-
-// max-w-screen-sm
+export default Nav;
