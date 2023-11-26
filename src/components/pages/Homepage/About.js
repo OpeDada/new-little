4e02a17e7947f6d5806429a59a2207d9f86de79b
OpeDada <opedada@gmail.com>
2023-11-26 23:57:29 +0900

    responsive design for about section images

--- src/components/pages/Homepage/About.js
+++ src/components/pages/Homepage/About.js
@@ -4,7 +4,7 @@ import owners2 from "../../../assets/M and Ad-b.jpg";
 const About = () => {
   return (
     <>
-      <div id="about" className="flex gap-20 my-24 px-20 xl:px-60">
+      <div id="about" className="lg:flex gap-20 my-24 px-10 xl:px-40">
         <div className="">
           <h1 className="text-2xl font-bold">Little Lemon</h1>
           <h2 className="text-lg font-bold pb-4">Chicago</h2>
@@ -17,18 +17,22 @@ const About = () => {
             thirty years later.
           </p>
         </div>
-        {/* <div className="relative w-80 h-80">
-          <img
-            src={owners}
-            className="absolute z-10 -left-16 -top-0 shadow-lg dark:shadow-black/20"
-            alt=""
-          />
-          <img
-            src={owners2}
-            className="absolute left-32 top-20 shadow-lg dark:shadow-black/20"
-            alt=""
-          />
-        </div> */}
+        <div className="flex">
+          <div className="py-8">
+            <img
+              src={owners}
+              className="shadow-lg dark:shadow-black/20 rounded-lg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src={owners2}
+              className="shadow-lg dark:shadow-black/20 rounded-lg"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </>
   );
