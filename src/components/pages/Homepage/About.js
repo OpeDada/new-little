8567c7bcb6cf20a119832b31b229cce724c2b54a
OpeDada <opedada@gmail.com>
2023-11-28 23:22:00 +0900

    responsive design for about section images

--- src/components/pages/Homepage/About.js
+++ src/components/pages/Homepage/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="">
           <h1 className="text-2xl font-bold">Little Lemon</h1>
           <h2 className="text-lg font-bold pb-4">Chicago</h2>
-          <p className="pr-8 max-w-lg sm:pb-4">
+          <p className="max-w-xs sm:pb-4">
             Little Lemon opened in 1995 by two Italian brothers, Adrian and
             Mario. Despite the city's diversity, the two brothers recognized the
             lack of Mediterranean cuisine in Chicago, and were inspired to bring
@@ -21,14 +21,14 @@ const About = () => {
           <div className="lg:relative bottom-8 ">
             <img
               src={owners}
-              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-sm"
+              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs"
               alt=""
             />
           </div>
           <div className="lg:absolute right-60 top-16 ">
             <img
               src={owners2}
-              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-sm"
+              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs"
               alt=""
             />
           </div>
