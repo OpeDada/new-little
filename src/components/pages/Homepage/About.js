25e149bd52797423d8ba07685da227fafe32557d
OpeDada <opedada@gmail.com>
2023-12-02 13:24:50 +0900

    responsive design for about section images

--- src/components/pages/Homepage/About.js
+++ src/components/pages/Homepage/About.js
@@ -6,12 +6,12 @@ const About = () => {
     <>
       <div
         id="about"
-        className="lg:flex justify-between my-24 p-10 xl:px-40"
+        className="lg:flex md:flex justify-between p-10 xl:px-40"
       >
         <div className="">
           <h1 className="text-2xl font-bold">Little Lemon</h1>
           <h2 className="text-lg font-bold pb-4">Chicago</h2>
-          <p className="lg:max-w-[50%] sm:pb-4">
+          <p className="lg:max-w-[50%] md:max-w-[70%] sm:pb-4">
             Little Lemon opened in 1995 by two Italian brothers, Adrian and
             Mario. Despite the city's diversity, the two brothers recognized the
             lack of Mediterranean cuisine in Chicago, and were inspired to bring
@@ -20,18 +20,18 @@ const About = () => {
             thirty years later.
           </p>
         </div>
-        <div className="lg:relative" >
-          <div className="lg:relative bottom-8">
+        <div className="lg:relative md:relative">
+          <div className="lg:relative bottom-8 md:relative">
             <img
               src={owners}
-              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs sm:mb-8"
+              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs md:max-w-xs sm:mb-8"
               alt=""
             />
           </div>
-          <div className="lg:absolute right-60 top-16">
+          <div className="lg:absolute lg:right-60 top-16 md:absolute md:right-24 ">
             <img
               src={owners2}
-              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs"
+              className="shadow-lg dark:shadow-black/20 rounded-lg lg:max-w-xs md:max-w-xs"
               alt=""
             />
           </div>
@@ -42,3 +42,5 @@ const About = () => {
 };
 
 export default About;
+
+// text-sm/[17px]
