44d313c7b6450378b20ac0e08265efd394a33bc9
OpeDada <opedada@gmail.com>
2023-12-03 23:13:36 +0900

    redesigning footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,51 @@
 const Footer = () => {
   return (
-    <footer class="p-10 xl:px-40 bg-green h-75 text-white">
-      <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" class="flex items-center">
+    <footer className="p-10 xl:px-40 bg-green h-75 text-white">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com/" className="flex items-center">
               <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                class="h-8 me-3"
-                alt="FlowBite Logo"
+                src={require("../assets/footer-logo.png")}
+                className="h-40 me-3"
+                alt="Footer Logo"
               />
-              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
-              </span>
             </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Resources
-              </h2>
-              <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                  <a href="https://flowbite.com/" class="hover:underline">
-                    Flowbite
-                  </a>
+              <h2 className="mb-6 font-semibold text-lg">Navigation</h2>
+
+              <ul className="font-medium">
+                <li>
+                  <a href="/">Home</a>
                 </li>
                 <li>
-                  <a href="https://tailwindcss.com/" class="hover:underline">
-                    Tailwind CSS
-                  </a>
+                  <a href="/about">About</a>
+                </li>
+                <li>
+                  <a href="/menu">Menu</a>
+                </li>
+                <li>
+                  <a href="/reservations">Reservations</a>
+                </li>
+                <li>
+                  <a href="/order">Order</a>
+                </li>
+                <li>
+                  <a href="/login">Login</a>
                 </li>
               </ul>
             </div>
             <div>
-              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                 Follow us
               </h2>
-              <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
                   <a
                     href="https://github.com/themesberg/flowbite"
-                    class="hover:underline "
+                    className="hover:underline "
                   >
                     Github
                   </a>
@@ -49,7 +53,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="https://discord.gg/4eeurUVvTy"
-                    class="hover:underline"
+                    className="hover:underline"
                   >
                     Discord
                   </a>
@@ -57,17 +61,17 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                 Legal
               </h2>
-              <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                  <a href="#" class="hover:underline">
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
                     Privacy Policy
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="hover:underline">
+                  <a href="#" className="hover:underline">
                     Terms &amp; Conditions
                   </a>
                 </li>
@@ -75,22 +79,22 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
-            <a href="https://flowbite.com/" class="hover:underline">
+            <a href="https://flowbite.com/" className="hover:underline">
               Flowbite™
             </a>
             . All Rights Reserved.
           </span>
-          <div class="flex mt-4 sm:justify-center sm:mt-0">
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
             <a
               href="#"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -102,14 +106,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span class="sr-only">Facebook page</span>
+              <span className="sr-only">Facebook page</span>
             </a>
             <a
               href="#"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -117,14 +121,14 @@ const Footer = () => {
               >
                 <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
               </svg>
-              <span class="sr-only">Discord community</span>
+              <span className="sr-only">Discord community</span>
             </a>
             <a
               href="#"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -136,14 +140,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span class="sr-only">Twitter page</span>
+              <span className="sr-only">Twitter page</span>
             </a>
             <a
               href="#"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -155,14 +159,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span class="sr-only">GitHub account</span>
+              <span className="sr-only">GitHub account</span>
             </a>
             <a
               href="#"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -174,7 +178,7 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span class="sr-only">Dribbble account</span>
+              <span className="sr-only">Dribbble account</span>
             </a>
           </div>
         </div>
@@ -185,69 +189,64 @@ const Footer = () => {
 
 export default Footer;
 
-
-
-
-
-
 <footer className="p-10 xl:px-40 bg-green h-75 text-white">
-        <div className="flex justify-between items-center pb-8">
-          <img
-            src={require("../assets/footer-logo.png")}
-            alt=""
-            className="h-48 pt-8"
-          />
-          <ul className="footer-links pt-8">
-            <p className="text-lg font-bold">Navigation</p>
-            <hr />
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/menu">Menu</a>
-            </li>
-            <li>
-              <a href="/reservations">Reservations</a>
-            </li>
-            <li>
-              <a href="/order">Order</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-          </ul>
+  <div className="flex justify-between items-center pb-8">
+    <img
+      src={require("../assets/footer-logo.png")}
+      alt=""
+      className="h-48 pt-8"
+    />
+    <ul className="footer-links pt-8">
+      <p className="text-lg font-bold">Navigation</p>
+      <hr />
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/menu">Menu</a>
+      </li>
+      <li>
+        <a href="/reservations">Reservations</a>
+      </li>
+      <li>
+        <a href="/order">Order</a>
+      </li>
+      <li>
+        <a href="/login">Login</a>
+      </li>
+    </ul>
 
-          <ul className="footer-links">
-            <p className="text-lg font-bold"> Contact</p>
-            <hr />
-            <li>
-              <a href="/">Address</a>
-            </li>
-            <li>
-              <a href="/about">Phone number</a>
-            </li>
-            <li>
-              <a href="/menu">Email</a>
-            </li>
-          </ul>
-          <ul className="footer-links">
-            <p className="text-lg font-bold">Social Media</p>
-            <hr />
-            <li>
-              <a href="/">Facebook</a>
-            </li>
-            <li>
-              <a href="/about">Linkedin</a>
-            </li>
-            <li>
-              <a href="/menu">Instagram</a>
-            </li>
-          </ul>
-        </div>
-        <p className="">
-          &copy; {new Date().getFullYear()} Little Lemon. All rights reserved.
-        </p>
-      </footer>
+    <ul className="footer-links">
+      <p className="text-lg font-bold"> Contact</p>
+      <hr />
+      <li>
+        <a href="/">Address</a>
+      </li>
+      <li>
+        <a href="/about">Phone number</a>
+      </li>
+      <li>
+        <a href="/menu">Email</a>
+      </li>
+    </ul>
+    <ul className="footer-links">
+      <p className="text-lg font-bold">Social Media</p>
+      <hr />
+      <li>
+        <a href="/">Facebook</a>
+      </li>
+      <li>
+        <a href="/about">Linkedin</a>
+      </li>
+      <li>
+        <a href="/menu">Instagram</a>
+      </li>
+    </ul>
+  </div>
+  <p className="">
+    &copy; {new Date().getFullYear()} Little Lemon. All rights reserved.
+  </p>
+</footer>;
