5f9a2b646ce1afe5a098c9afe9c4af562dd0690a
OpeDada <opedada@gmail.com>
2023-12-10 17:41:04 +0900

    confirmation page design

--- src/components/pages/ConfirmedBooking.js
+++ src/components/pages/ConfirmedBooking.js
@@ -5,7 +5,7 @@ const ConfirmedBooking = () => {
       <img
         src={require("../../assets/restaurant.jpg")}
         alt="greek salad"
-        className="w-full lg:h-[650px]"
+        className="w-full lg:h-[620px]"
       />
       <div className="bg-white lg:p-12 md:p-12 sm:p-4 rounded border-4 border-double absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 className="text-2xl font-semibold mb-4">Booking Confirmed</h1>
